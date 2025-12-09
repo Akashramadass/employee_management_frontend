26dfe4a00b8d05207e70b11fb9f2c5fa2e52b1d8
akashramadass <akashramadass2001@gmail.com>
2025-12-09 12:12:38 +0530

    updated frontend files

--- src/api/EmployeeService.js
+++ src/api/EmployeeService.js
@@ -3,7 +3,12 @@ import axios from "axios";
 const API_URL = "https://employee-management-backend-2djx.onrender.com/api/employees";
 
 export const getAllEmployees = () => axios.get(API_URL);
-export const getEmployeeById = (id) => axios.get(`${API_URL}/${id}`);
-export const createEmployee = (employee) => axios.post(API_URL, employee);
-export const updateEmployee = (id, employee) => axios.put(`${API_URL}/${id}`, employee);
-export const deleteEmployee = (id) => axios.delete(`${API_URL}/${id}`);
+
+export const createEmployee = (employee) =>
+  axios.post(API_URL, employee);
+
+export const updateEmployee = (id, employee) =>
+  axios.put(`${API_URL}/${id}`, employee);
+
+export const deleteEmployee = (id) =>
+  axios.delete(`${API_URL}/${id}`);
